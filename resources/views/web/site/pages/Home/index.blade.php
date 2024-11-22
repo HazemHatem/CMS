1386a27fc5ae74e40a64f62b802148b333916abf
Ahmed abdeldaym <ahmedabdeldaem01000@gmail.com>
2024-11-22 19:17:28 +0200

    profile controller

--- resources/views/web/site/pages/Home/index.blade.php
+++ resources/views/web/site/pages/Home/index.blade.php
@@ -24,6 +24,18 @@
     </section>
     <!-- End landing -->
 
+    <div>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+        <h1>ِAhmed solve prplem calle</h1>
+    </div>
     <div class="categories col-12 ">
         <!-- sm 10 -->
         <ul class="col-sm-10 col-lg-7">
